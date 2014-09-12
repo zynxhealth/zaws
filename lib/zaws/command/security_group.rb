7002991f6cbefa34fb75b3826171eb139a31408e
Aslan Brooke <abrooke@zynx.com>
2014-09-12 15:06:17 -0700

    Aslan - Reformatting files.

--- lib/zaws/command/security_group.rb
+++ lib/zaws/command/security_group.rb
@@ -2,91 +2,91 @@ require 'thor'
 
 module ZAWS
   module Command
-	class Security_Group < Thor 
-	  class_option :region, :type => :string, :desc => "AWS Region", :banner => "<region>",  :aliases => :r, :required => true
-	  class_option :verbose, :type => :boolean, :desc => "Verbose outout", :aliases => :d, :default => false
+    class Security_Group < Thor
+      class_option :region, :type => :string, :desc => "AWS Region", :banner => "<region>", :aliases => :r, :required => true
+      class_option :verbose, :type => :boolean, :desc => "Verbose outout", :aliases => :d, :default => false
 
-	  desc "view","View security groups."
-	  option :viewtype, :type => :string, :desc => "View type, json or table", :banner => "<viewtype>", :aliases => :w, :default => "table"
-	  option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>",  :aliases => :v, :default => nil
-	  def view
-		aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
-		aws.ec2.security_group.view(options[:region],options[:viewtype],$stdout,(options[:verbose]?$stdout:nil),options[:vpcid])
-	  end
+      desc "view", "View security groups."
+      option :viewtype, :type => :string, :desc => "View type, json or table", :banner => "<viewtype>", :aliases => :w, :default => "table"
+      option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>", :aliases => :v, :default => nil
+      def view
+        aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
+        aws.ec2.security_group.view(options[:region], options[:viewtype], $stdout, (options[:verbose] ? $stdout : nil), options[:vpcid])
+      end
 
-	  desc "exists_by_name GROUP_NAME","Determine if a security group exists by name GROUP_NAME."
-	  option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>",  :aliases => :v, :default => nil
-	  def exists_by_name(group_name) 
-		aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
-		aws.ec2.security_group.exists(options[:region],$stdout,(options[:verbose]?$stdout:nil),options[:vpcid],group_name)
-	  end
+      desc "exists_by_name GROUP_NAME", "Determine if a security group exists by name GROUP_NAME."
+      option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>", :aliases => :v, :default => nil
+      def exists_by_name(group_name)
+        aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
+        aws.ec2.security_group.exists(options[:region], $stdout, (options[:verbose] ? $stdout : nil), options[:vpcid], group_name)
+      end
 
-	  desc "declare GROUP_NAME DESCRIPTION","Declare a new security group GROUP_NAME, but skip creating it if it exists."
-	  option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>",  :aliases => :v, :default => nil
-      option :nagios, :type => :boolean, :desc => "Returns a nagios check result",  :aliases => :n, :default => false
-	  option :undofile, :type => :string, :desc => "File for undo commands", :banner => "<undofile>", :aliases => :f, :default => nil
-	  def declare(group_name,description) 
-		aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
-		exitcode = aws.ec2.security_group.declare(options[:region],options[:vpcid],group_name,description,options[:nagios],$stdout,(options[:verbose]?$stdout:nil),options[:undofile])
+      desc "declare GROUP_NAME DESCRIPTION", "Declare a new security group GROUP_NAME, but skip creating it if it exists."
+      option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>", :aliases => :v, :default => nil
+      option :nagios, :type => :boolean, :desc => "Returns a nagios check result", :aliases => :n, :default => false
+      option :undofile, :type => :string, :desc => "File for undo commands", :banner => "<undofile>", :aliases => :f, :default => nil
+      def declare(group_name, description)
+        aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
+        exitcode = aws.ec2.security_group.declare(options[:region], options[:vpcid], group_name, description, options[:nagios], $stdout, (options[:verbose] ? $stdout : nil), options[:undofile])
         exit exitcode
-	  end
+      end
 
-	  desc "delete GROUP_NAME","Delete a new security group GROUP_NAME, but skip it if it does not exist."
-	  option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>",  :aliases => :v, :default => nil
-	  def delete(group_name) 
-		aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
-		aws.ec2.security_group.delete(options[:region],$stdout,(options[:verbose]?$stdout:nil),options[:vpcid],group_name)
-	  end
+      desc "delete GROUP_NAME", "Delete a new security group GROUP_NAME, but skip it if it does not exist."
+      option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>", :aliases => :v, :default => nil
+      def delete(group_name)
+        aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
+        aws.ec2.security_group.delete(options[:region], $stdout, (options[:verbose] ? $stdout : nil), options[:vpcid], group_name)
+      end
 
-	  desc "ingress_group_exists TARGET_GROUP_NAME SOURCE_GROUP_NAME PROTOCOL PORT","Determine if an ingress security group rule exists."
-	  option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>",  :aliases => :v, :default => nil
-	  def ingress_group_exists(target,source,protocol,port) 
-		aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
-		aws.ec2.security_group.ingress_group_exists(options[:region],options[:vpcid],target,source,protocol,port,$stdout,(options[:verbose]?$stdout:nil))
-	  end
+      desc "ingress_group_exists TARGET_GROUP_NAME SOURCE_GROUP_NAME PROTOCOL PORT", "Determine if an ingress security group rule exists."
+      option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>", :aliases => :v, :default => nil
+      def ingress_group_exists(target, source, protocol, port)
+        aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
+        aws.ec2.security_group.ingress_group_exists(options[:region], options[:vpcid], target, source, protocol, port, $stdout, (options[:verbose] ? $stdout : nil))
+      end
 
-	  desc "ingress_cidr_exists TARGET_GROUP_NAME CIDR PROTOCOL PORT","Determine if an ingress CIDR rule exists."
-	  option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>",  :aliases => :v, :default => nil
-	  def ingress_cidr_exists(target,cidr,protocol,port) 
-		aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
-		aws.ec2.security_group.ingress_cidr_exists(options[:region],options[:vpcid],target,cidr,protocol,port,$stdout,(options[:verbose]?$stdout:nil))
-	  end
+      desc "ingress_cidr_exists TARGET_GROUP_NAME CIDR PROTOCOL PORT", "Determine if an ingress CIDR rule exists."
+      option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>", :aliases => :v, :default => nil
+      def ingress_cidr_exists(target, cidr, protocol, port)
+        aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
+        aws.ec2.security_group.ingress_cidr_exists(options[:region], options[:vpcid], target, cidr, protocol, port, $stdout, (options[:verbose] ? $stdout : nil))
+      end
 
-	  desc "declare_ingress_group TARGET_GROUP_NAME SOURCE_GROUP_NAME PROTOCOL PORT","Declare an ingress security group rule."
-	  option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>",  :aliases => :v, :default => nil
-      option :nagios, :type => :boolean, :desc => "Returns a nagios check result",  :aliases => :n, :default => false
+      desc "declare_ingress_group TARGET_GROUP_NAME SOURCE_GROUP_NAME PROTOCOL PORT", "Declare an ingress security group rule."
+      option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>", :aliases => :v, :default => nil
+      option :nagios, :type => :boolean, :desc => "Returns a nagios check result", :aliases => :n, :default => false
       option :undofile, :type => :string, :desc => "File for undo commands", :banner => "<undofile>", :aliases => :f, :default => nil
-	  def declare_ingress_group(target,source,protocol,port) 
-		aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
-		exitcode = aws.ec2.security_group.declare_ingress_group(options[:region],options[:vpcid],target,source,protocol,port,options[:nagios],$stdout,(options[:verbose]?$stdout:nil),options[:undofile])
-		exit exitcode
-	  end
+      def declare_ingress_group(target, source, protocol, port)
+        aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
+        exitcode = aws.ec2.security_group.declare_ingress_group(options[:region], options[:vpcid], target, source, protocol, port, options[:nagios], $stdout, (options[:verbose] ? $stdout : nil), options[:undofile])
+        exit exitcode
+      end
 
-	  desc "declare_ingress_cidr TARGET_GROUP_NAME CIDR PROTOCOL PORT","Declare an ingress CIDR rule."
-	  option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>",  :aliases => :v, :default => nil
-      option :nagios, :type => :boolean, :desc => "Returns a nagios check result",  :aliases => :n, :default => false
+      desc "declare_ingress_cidr TARGET_GROUP_NAME CIDR PROTOCOL PORT", "Declare an ingress CIDR rule."
+      option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>", :aliases => :v, :default => nil
+      option :nagios, :type => :boolean, :desc => "Returns a nagios check result", :aliases => :n, :default => false
       option :undofile, :type => :string, :desc => "File for undo commands", :banner => "<undofile>", :aliases => :f, :default => nil
-	  def declare_ingress_cidr(target,cidr,protocol,port) 
-		aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
-		exitcode = aws.ec2.security_group.declare_ingress_cidr(options[:region],options[:vpcid],target,cidr,protocol,port,options[:nagios],$stdout,(options[:verbose]?$stdout:nil),options[:undofile])
-		exit exitcode
-	  end
+      def declare_ingress_cidr(target, cidr, protocol, port)
+        aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
+        exitcode = aws.ec2.security_group.declare_ingress_cidr(options[:region], options[:vpcid], target, cidr, protocol, port, options[:nagios], $stdout, (options[:verbose] ? $stdout : nil), options[:undofile])
+        exit exitcode
+      end
 
-	  desc "delete_ingress_group TARGET_GROUP_NAME SOURCE_GROUP_NAME PROTOCOL PORT","Delete an ingress security group rule."
-	  option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>",  :aliases => :v, :default => nil
-	  def delete_ingress_group(target,source,protocol,port) 
-		aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
-		aws.ec2.security_group.delete_ingress_group(options[:region],options[:vpcid],target,source,protocol,port,$stdout,(options[:verbose]?$stdout:nil))
-	  end
+      desc "delete_ingress_group TARGET_GROUP_NAME SOURCE_GROUP_NAME PROTOCOL PORT", "Delete an ingress security group rule."
+      option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>", :aliases => :v, :default => nil
+      def delete_ingress_group(target, source, protocol, port)
+        aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
+        aws.ec2.security_group.delete_ingress_group(options[:region], options[:vpcid], target, source, protocol, port, $stdout, (options[:verbose] ? $stdout : nil))
+      end
 
-	  desc "delete_ingress_cidr TARGET_GROUP_NAME CIDR PROTOCOL PORT","Delete an ingress security cidr rule."
-	  option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>",  :aliases => :v, :default => nil
-	  def delete_ingress_cidr(target,cidr,protocol,port) 
-		aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
-		aws.ec2.security_group.delete_ingress_cidr(options[:region],options[:vpcid],target,cidr,protocol,port,$stdout,(options[:verbose]?$stdout:nil))
-	  end
+      desc "delete_ingress_cidr TARGET_GROUP_NAME CIDR PROTOCOL PORT", "Delete an ingress security cidr rule."
+      option :vpcid, :type => :string, :desc => "AWS VPC id", :banner => "<vpcid>", :aliases => :v, :default => nil
+      def delete_ingress_cidr(target, cidr, protocol, port)
+        aws=(ZAWS::AWS.new(ZAWS::Helper::Shell.new))
+        aws.ec2.security_group.delete_ingress_cidr(options[:region], options[:vpcid], target, cidr, protocol, port, $stdout, (options[:verbose] ? $stdout : nil))
+      end
 
-	end
+    end
   end
 end
 
--- lib/zaws/ec2/security_group.rb
+++ lib/zaws/ec2/security_group.rb
@@ -4,154 +4,154 @@ require 'timeout'
 
 module ZAWS
   module EC2Services
-	class SecurityGroup 
+    class SecurityGroup
 
-	  def initialize(shellout,aws)
-		@shellout=shellout
-		@aws=aws
-	  end
+      def initialize(shellout, aws)
+        @shellout=shellout
+        @aws=aws
+      end
 
-	  def view(region,view,textout=nil,verbose=nil,vpcid=nil,groupname=nil,groupid=nil,perm_groupid=nil,perm_protocol=nil,perm_toport=nil,cidr=nil)
-		comline="aws --output #{view} --region #{region} ec2 describe-security-groups"
-		if vpcid || groupname
-		  comline = comline + " --filter"
-		end
-		comline = comline + " 'Name=vpc-id,Values=#{vpcid}'" if vpcid 
-		comline = comline + " 'Name=group-name,Values=#{groupname}'" if groupname
+      def view(region, view, textout=nil, verbose=nil, vpcid=nil, groupname=nil, groupid=nil, perm_groupid=nil, perm_protocol=nil, perm_toport=nil, cidr=nil)
+        comline="aws --output #{view} --region #{region} ec2 describe-security-groups"
+        if vpcid || groupname
+          comline = comline + " --filter"
+        end
+        comline = comline + " 'Name=vpc-id,Values=#{vpcid}'" if vpcid
+        comline = comline + " 'Name=group-name,Values=#{groupname}'" if groupname
         comline = comline + " 'Name=group-id,Values=#{groupid}'" if groupid
         comline = comline + " 'Name=ip-permission.group-id,Values=#{perm_groupid}'" if perm_groupid
-        comline = comline + " 'Name=ip-permission.cidr,Values=#{cidr}'" if cidr 
-	    comline = comline + " 'Name=ip-permission.protocol,Values=#{perm_protocol}'" if perm_protocol
+        comline = comline + " 'Name=ip-permission.cidr,Values=#{cidr}'" if cidr
+        comline = comline + " 'Name=ip-permission.protocol,Values=#{perm_protocol}'" if perm_protocol
         comline = comline + " 'Name=ip-permission.to-port,Values=#{perm_toport}'" if perm_toport
-		sgroups=@shellout.cli(comline,verbose)
-		textout.puts(sgroups) if textout
-		return sgroups
-	  end
+        sgroups=@shellout.cli(comline, verbose)
+        textout.puts(sgroups) if textout
+        return sgroups
+      end
 
-	  def exists(region,textout=nil,verbose=nil,vpcid,groupname)
-		sgroups=JSON.parse(view(region,'json',nil,verbose,vpcid,groupname))
-		val = (sgroups["SecurityGroups"].count == 1)
-		sgroupid = val ? sgroups["SecurityGroups"][0]["GroupId"] : nil
-		textout.puts val.to_s if textout
-		return val, sgroupid 
-	  end
+      def exists(region, textout=nil, verbose=nil, vpcid, groupname)
+        sgroups=JSON.parse(view(region, 'json', nil, verbose, vpcid, groupname))
+        val = (sgroups["SecurityGroups"].count == 1)
+        sgroupid = val ? sgroups["SecurityGroups"][0]["GroupId"] : nil
+        textout.puts val.to_s if textout
+        return val, sgroupid
+      end
 
-	  def declare(region,vpcid,groupname,description,nagios,textout=nil,verbose=nil,ufile=nil)
-		if ufile
-          ZAWS::Helper::ZFile.prepend("zaws security_group delete #{groupname} --region #{region} --vpcid #{vpcid} $XTRA_OPTS",'#Delete security group',ufile)
-		end
-		sgroup_exists,sgroupid = exists(region,nil,verbose,vpcid,groupname) 
-		return ZAWS::Helper::Output.binary_nagios_check(sgroup_exists,"OK: Security Group Exists.","CRITICAL: Security Group Does Not Exist.",textout) if nagios
-		if not sgroup_exists
-		  comline="aws --output json --region #{region} ec2 create-security-group --vpc-id #{vpcid} --group-name #{groupname} --description '#{description}'"
-		  sgroup=JSON.parse(@shellout.cli(comline,verbose))
-		  textout.puts "Security Group Created." if sgroup["return"] == "true"
-		else
-		  textout.puts "Security Group Exists Already. Skipping Creation."
-		end
-		return 0
-	  end
+      def declare(region, vpcid, groupname, description, nagios, textout=nil, verbose=nil, ufile=nil)
+        if ufile
+          ZAWS::Helper::ZFile.prepend("zaws security_group delete #{groupname} --region #{region} --vpcid #{vpcid} $XTRA_OPTS", '#Delete security group', ufile)
+        end
+        sgroup_exists, sgroupid = exists(region, nil, verbose, vpcid, groupname)
+        return ZAWS::Helper::Output.binary_nagios_check(sgroup_exists, "OK: Security Group Exists.", "CRITICAL: Security Group Does Not Exist.", textout) if nagios
+        if not sgroup_exists
+          comline="aws --output json --region #{region} ec2 create-security-group --vpc-id #{vpcid} --group-name #{groupname} --description '#{description}'"
+          sgroup=JSON.parse(@shellout.cli(comline, verbose))
+          textout.puts "Security Group Created." if sgroup["return"] == "true"
+        else
+          textout.puts "Security Group Exists Already. Skipping Creation."
+        end
+        return 0
+      end
 
-      def id_by_name(region,textout=nil,verbose=nil,vpcid,groupname)
-		sgroups=JSON.parse(view(region,'json',nil,verbose,vpcid,groupname))
-		group_id= sgroups["SecurityGroups"].count == 1 ? sgroups["SecurityGroups"][0]["GroupId"] : nil
-		raise "More than one security group found when looking up id by name." if sgroups["SecurityGroups"].count > 1 
-		textout.puts group_id if textout
-		return group_id
-	  end
+      def id_by_name(region, textout=nil, verbose=nil, vpcid, groupname)
+        sgroups=JSON.parse(view(region, 'json', nil, verbose, vpcid, groupname))
+        group_id= sgroups["SecurityGroups"].count == 1 ? sgroups["SecurityGroups"][0]["GroupId"] : nil
+        raise "More than one security group found when looking up id by name." if sgroups["SecurityGroups"].count > 1
+        textout.puts group_id if textout
+        return group_id
+      end
 
-    
-	  def delete(region,textout=nil,verbose=nil,vpcid,groupname)
-		groupid=id_by_name(region,nil,nil,vpcid,groupname)
-		if groupid 
-		  comline="aws --region #{region} ec2 delete-security-group --group-id #{groupid}"
-		  sgroup=JSON.parse(@shellout.cli(comline,verbose))
-		  textout.puts "Security Group deleted." if sgroup["return"] == "true"
-		else
-		  textout.puts "Security Group does not exist. Skipping deletion."
-		end
-	  end
 
-      def ingress_group_exists(region,vpcid,target,source,protocol,port,textout=nil,verbose=nil) 
-        targetid=id_by_name(region,nil,nil,vpcid,target)
-        sourceid=id_by_name(region,nil,nil,vpcid,source)
+      def delete(region, textout=nil, verbose=nil, vpcid, groupname)
+        groupid=id_by_name(region, nil, nil, vpcid, groupname)
+        if groupid
+          comline="aws --region #{region} ec2 delete-security-group --group-id #{groupid}"
+          sgroup=JSON.parse(@shellout.cli(comline, verbose))
+          textout.puts "Security Group deleted." if sgroup["return"] == "true"
+        else
+          textout.puts "Security Group does not exist. Skipping deletion."
+        end
+      end
+
+      def ingress_group_exists(region, vpcid, target, source, protocol, port, textout=nil, verbose=nil)
+        targetid=id_by_name(region, nil, nil, vpcid, target)
+        sourceid=id_by_name(region, nil, nil, vpcid, source)
         if targetid && sourceid
-          sgroups=JSON.parse(view(region,'json',nil,verbose,vpcid,nil,targetid,sourceid,protocol,port))
-		  val = (sgroups["SecurityGroups"].count > 0)
-          textout.puts val.to_s if textout
-		  return val, targetid, sourceid 
-		end
-	  end
-
-	  def ingress_cidr_exists(region,vpcid,target,cidr,protocol,port,textout=nil,verbose=nil) 
-        targetid=id_by_name(region,nil,nil,vpcid,target)
-        if targetid 
-          sgroups=JSON.parse(view(region,'json',nil,verbose,vpcid,nil,targetid,nil,protocol,port,cidr))
-		  if (sgroups["SecurityGroups"].count > 0)
-            # Additionally filter out the sgroups that do not have the cidr and port in the same ip permissions
-		    sgroups["SecurityGroups"]=sgroups["SecurityGroups"].select {|x| x['IpPermissions'].any? {|y| y['ToPort'] and y['FromPort'] and y['ToPort']==port.to_i and y['FromPort']==port.to_i and y['IpRanges'].any? {|z| z['CidrIp']=="#{cidr}" }}} 
-		  end 
+          sgroups=JSON.parse(view(region, 'json', nil, verbose, vpcid, nil, targetid, sourceid, protocol, port))
           val = (sgroups["SecurityGroups"].count > 0)
           textout.puts val.to_s if textout
-		  return val, targetid 
-		end
-	  end
+          return val, targetid, sourceid
+        end
+      end
 
-	  def declare_ingress_group(region,vpcid,target,source,protocol,port,nagios,textout=nil,verbose=nil,ufile=nil) 
-		if ufile
-		  ZAWS::Helper::ZFile.prepend("zaws security_group delete_ingress_group #{target} #{source} #{protocol} #{port} --region #{region} --vpcid #{vpcid} $XTRA_OPTS",'#Delete security group ingress group rule',ufile)
-		end
-		ingress_exists,targetid,sourceid = ingress_group_exists(region,vpcid,target,source,protocol,port,nil,verbose)
-		return ZAWS::Helper::Output.binary_nagios_check(ingress_exists,"OK: Security group ingress group rule exists.","CRITICAL: Security group ingress group rule does not exist.",textout) if nagios
-		if not ingress_exists 
+      def ingress_cidr_exists(region, vpcid, target, cidr, protocol, port, textout=nil, verbose=nil)
+        targetid=id_by_name(region, nil, nil, vpcid, target)
+        if targetid
+          sgroups=JSON.parse(view(region, 'json', nil, verbose, vpcid, nil, targetid, nil, protocol, port, cidr))
+          if (sgroups["SecurityGroups"].count > 0)
+            # Additionally filter out the sgroups that do not have the cidr and port in the same ip permissions
+            sgroups["SecurityGroups"]=sgroups["SecurityGroups"].select { |x| x['IpPermissions'].any? { |y| y['ToPort'] and y['FromPort'] and y['ToPort']==port.to_i and y['FromPort']==port.to_i and y['IpRanges'].any? { |z| z['CidrIp']=="#{cidr}" } } }
+          end
+          val = (sgroups["SecurityGroups"].count > 0)
+          textout.puts val.to_s if textout
+          return val, targetid
+        end
+      end
+
+      def declare_ingress_group(region, vpcid, target, source, protocol, port, nagios, textout=nil, verbose=nil, ufile=nil)
+        if ufile
+          ZAWS::Helper::ZFile.prepend("zaws security_group delete_ingress_group #{target} #{source} #{protocol} #{port} --region #{region} --vpcid #{vpcid} $XTRA_OPTS", '#Delete security group ingress group rule', ufile)
+        end
+        ingress_exists, targetid, sourceid = ingress_group_exists(region, vpcid, target, source, protocol, port, nil, verbose)
+        return ZAWS::Helper::Output.binary_nagios_check(ingress_exists, "OK: Security group ingress group rule exists.", "CRITICAL: Security group ingress group rule does not exist.", textout) if nagios
+        if not ingress_exists
           comline="aws --region #{region} ec2 authorize-security-group-ingress --group-id #{targetid} --source-security-group-owner-id #{sourceid} --protocol #{protocol} --port #{port}"
-		  ingressrule=JSON.parse(@shellout.cli(comline,verbose))
-		  textout.puts "Ingress group rule created." if ingressrule["return"] == "true"
-		else
+          ingressrule=JSON.parse(@shellout.cli(comline, verbose))
+          textout.puts "Ingress group rule created." if ingressrule["return"] == "true"
+        else
           textout.puts "Ingress group rule not created. Exists already."
-		end
-		return 0
-	  end
+        end
+        return 0
+      end
 
-	  def declare_ingress_cidr(region,vpcid,target,cidr,protocol,port,nagios,textout=nil,verbose=nil,ufile=nil) 
-		if ufile
-		  ZAWS::Helper::ZFile.prepend("zaws security_group delete_ingress_cidr #{target} #{cidr} #{protocol} #{port} --region #{region} --vpcid #{vpcid} $XTRA_OPTS",'#Delete cidr ingress group rule',ufile)
-		end
-		ingress_exists,targetid = ingress_cidr_exists(region,vpcid,target,cidr,protocol,port,nil,verbose)
-		return ZAWS::Helper::Output.binary_nagios_check(ingress_exists,"OK: Security group ingress cidr rule exists.","CRITICAL: Security group ingress cidr rule does not exist.",textout) if nagios
-		if not ingress_exists 
+      def declare_ingress_cidr(region, vpcid, target, cidr, protocol, port, nagios, textout=nil, verbose=nil, ufile=nil)
+        if ufile
+          ZAWS::Helper::ZFile.prepend("zaws security_group delete_ingress_cidr #{target} #{cidr} #{protocol} #{port} --region #{region} --vpcid #{vpcid} $XTRA_OPTS", '#Delete cidr ingress group rule', ufile)
+        end
+        ingress_exists, targetid = ingress_cidr_exists(region, vpcid, target, cidr, protocol, port, nil, verbose)
+        return ZAWS::Helper::Output.binary_nagios_check(ingress_exists, "OK: Security group ingress cidr rule exists.", "CRITICAL: Security group ingress cidr rule does not exist.", textout) if nagios
+        if not ingress_exists
           comline="aws --region #{region} ec2 authorize-security-group-ingress --group-id #{targetid} --cidr #{cidr} --protocol #{protocol} --port #{port}"
-		  ingressrule=JSON.parse(@shellout.cli(comline,verbose))
-		  textout.puts "Ingress cidr rule created." if ingressrule["return"] == "true"
-		else
+          ingressrule=JSON.parse(@shellout.cli(comline, verbose))
+          textout.puts "Ingress cidr rule created." if ingressrule["return"] == "true"
+        else
           textout.puts "Ingress cidr rule not created. Exists already."
-		end
-		return 0
-	  end
+        end
+        return 0
+      end
 
-	  def delete_ingress_group(region,vpcid,target,source,protocol,port,textout=nil,verbose=nil) 
-		ingress_exists,targetid,sourceid = ingress_group_exists(region,vpcid,target,source,protocol,port,nil,verbose)
-		if ingress_exists 
+      def delete_ingress_group(region, vpcid, target, source, protocol, port, textout=nil, verbose=nil)
+        ingress_exists, targetid, sourceid = ingress_group_exists(region, vpcid, target, source, protocol, port, nil, verbose)
+        if ingress_exists
           comline="aws --region #{region} ec2 revoke-security-group-ingress --group-id #{targetid} --source-security-group-owner-id #{sourceid} --protocol #{protocol} --port #{port}"
-		  val=JSON.parse(@shellout.cli(comline,verbose))
-		  textout.puts "Security group ingress group rule deleted." if val["return"] == "true"
-		else
+          val=JSON.parse(@shellout.cli(comline, verbose))
+          textout.puts "Security group ingress group rule deleted." if val["return"] == "true"
+        else
           textout.puts "Security group ingress group rule does not exist. Skipping deletion."
-		end
-	  end
+        end
+      end
 
-	  def delete_ingress_cidr(region,vpcid,target,cidr,protocol,port,textout=nil,verbose=nil) 
-		ingress_exists,targetid = ingress_cidr_exists(region,vpcid,target,cidr,protocol,port,nil,verbose)
-		if ingress_exists 
+      def delete_ingress_cidr(region, vpcid, target, cidr, protocol, port, textout=nil, verbose=nil)
+        ingress_exists, targetid = ingress_cidr_exists(region, vpcid, target, cidr, protocol, port, nil, verbose)
+        if ingress_exists
           comline="aws --region #{region} ec2 revoke-security-group-ingress --group-id #{targetid} --cidr #{cidr} --protocol #{protocol} --port #{port}"
-		  val=JSON.parse(@shellout.cli(comline,verbose))
-		  textout.puts "Security group ingress cidr rule deleted." if val["return"] == "true"
-		else
+          val=JSON.parse(@shellout.cli(comline, verbose))
+          textout.puts "Security group ingress cidr rule deleted." if val["return"] == "true"
+        else
           textout.puts "Security group ingress cidr rule does not exist. Skipping deletion."
-		end
-	  end
+        end
+      end
 
-	 end
+    end
   end
 end
 
