656ccc10aacb13b9377dbbccf481e83e8c169c24
Aslan <abrooke@zynx.com>
2014-11-15 15:45:21 -0800

    Aslan - increased timeout a little to account for travis timeouts.

--- feature/support/env.rb
+++ feature/support/env.rb
@@ -6,7 +6,7 @@ require 'aruba/cucumber'
 require 'aruba-doubles/cucumber'
 
 Before do
-  @aruba_timeout_seconds = 8
+  @aruba_timeout_seconds = 10
   # This is using the aruba helper, 
   # cf. https://github.com/cucumber/aruba/blob/master/lib/aruba/api.rb
   set_env('COVERAGE', 'true')
